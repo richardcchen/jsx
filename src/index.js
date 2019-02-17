fdf08e26473205512bfb95a9cb23de75b11d1ddf
Richard Chen <richard.c.chen@gmail.com>
2019-02-17 17:36:17 -0500

    reviewing jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div> Hi there!</div>
+  return (
+    <div>
+      <label class="label" for="name">Enter Name:</label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+    </div>
+  )
 }
 
 ReactDOM.render(
